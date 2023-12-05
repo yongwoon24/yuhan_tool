f2e406d8693a8ad29c31864ae21f28e9e0f00c54
hungry <hungry1358@naver.com>
2023-12-05 16:31:08 +0900

    그림판

---
+++ shim/drawing/sln_draw/drawing/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.지우개ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장ToolStripMenuItem,
+            this.불러오기ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+            // 
+            // 불러오기ToolStripMenuItem
+            // 
+            this.불러오기ToolStripMenuItem.Name = "불러오기ToolStripMenuItem";
+            this.불러오기ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.불러오기ToolStripMenuItem.Text = "불러오기";
+            this.불러오기ToolStripMenuItem.Click += new System.EventHandler(this.불러오기ToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.redoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // redoToolStripMenuItem
+            // 
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.redoToolStripMenuItem.Text = "Redo";
+            this.redoToolStripMenuItem.Click += new System.EventHandler(this.redoToolStripMenuItem_Click);
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectangleToolStripMenuItem,
+            this.circleToolStripMenuItem,
+            this.lineToolStripMenuItem,
+            this.지우개ToolStripMenuItem});
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.toolToolStripMenuItem.Text = "Tool";
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rectangleToolStripMenuItem.Text = "사각형";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.rectangleToolStripMenuItem_Click);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.circleToolStripMenuItem.Text = "원";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lineToolStripMenuItem.Text = "선";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 421);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // 지우개ToolStripMenuItem
+            // 
+            this.지우개ToolStripMenuItem.Name = "지우개ToolStripMenuItem";
+            this.지우개ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.지우개ToolStripMenuItem.Text = "지우개";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 지우개ToolStripMenuItem;
+    }
+}
+
---
+++ shim/drawing/sln_draw/drawing/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace drawing
+{
+    public partial class Form1 : Form
+    {
+
+        private static Point clickPoint;
+        private static Point UpPoint;
+        private static Bitmap OriginalBmp;
+        private static Bitmap DrawBmp;
+        private static Rectangle imgRect;
+
+
+        public static PaintTools toolType { get; set; }
+        public enum PaintTools
+        {
+            IDLE = default,
+            DrawLine,
+            DrawRectangle,
+            DrawCircle,
+            Eraser
+        }
+
+        public List<Rectangle> listRect = new List<Rectangle>();
+        public List<Rectangle> tempRect = new List<Rectangle>();
+        public List<PaintTools> listTool = new List<PaintTools>();
+        public List<PaintTools> tempTool = new List<PaintTools>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //White BackgroundImage Load
+            pictureBox1.Image = new Bitmap(Application.StartupPath + @"\DefaultBackground.png");
+            OriginalBmp = (Bitmap)pictureBox1.Image;
+            imgRect = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolType = PaintTools.DrawRectangle;
+        }
+
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolType = PaintTools.DrawCircle;
+        }
+
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolType = PaintTools.DrawLine;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //마우스 클릭 위치 저장
+                clickPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                float w = Math.Abs(clickPoint.X - e.X);
+                float h = Math.Abs(clickPoint.Y - e.Y);
+
+                Pen pn = new Pen(Color.Black);
+                pn.Width = 3;
+                Graphics g = pictureBox1.CreateGraphics();
+                pictureBox1.Refresh();
+
+                if (toolType == PaintTools.DrawRectangle) //사각형 그리기
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y) g.DrawRectangle(pn, clickPoint.X, clickPoint.Y, w, h);
+                        else g.DrawRectangle(pn, clickPoint.X, e.Y, w, h);
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y) g.DrawRectangle(pn, e.X, clickPoint.Y, w, h);
+                        else g.DrawRectangle(pn, e.X, e.Y, w, h);
+                    }
+                }
+                else if (toolType == PaintTools.DrawCircle) //원형 그리기
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y) g.DrawEllipse(pn, clickPoint.X, clickPoint.Y, w, h);
+                        else g.DrawEllipse(pn, clickPoint.X, e.Y, w, h);
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y) g.DrawEllipse(pn, e.X, clickPoint.Y, w, h);
+                        else g.DrawEllipse(pn, e.X, e.Y, w, h);
+                    }
+                }
+                else if (toolType == PaintTools.DrawLine) //선형 그리기
+                {
+                    g.DrawLine(pn, clickPoint.X, clickPoint.Y, e.X, e.Y);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                UpPoint.X = e.X;
+                UpPoint.Y = e.Y;
+
+                float w = Math.Abs(clickPoint.X - e.X);
+                float h = Math.Abs(clickPoint.Y - e.Y);
+
+                Pen pn = new Pen(Color.Black);
+                pn.Width = 3;
+                Rectangle rect = new Rectangle();
+                Graphics g = pictureBox1.CreateGraphics();
+
+                if (toolType == PaintTools.DrawRectangle) //사각형 그리기
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y) rect = new Rectangle(clickPoint.X, clickPoint.Y, (int)w, (int)h);
+                        else rect = new Rectangle(clickPoint.X, e.Y, (int)w, (int)h);
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y) rect = new Rectangle(e.X, clickPoint.Y, (int)w, (int)h);
+                        else rect = new Rectangle(e.X, e.Y, (int)w, (int)h);
+                    }
+                }
+                else if (toolType == PaintTools.DrawCircle) //원형 그리기
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y) rect = new Rectangle(clickPoint.X, clickPoint.Y, (int)w, (int)h);
+                        else rect = new Rectangle(clickPoint.X, e.Y, (int)w, (int)h);
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y) rect = new Rectangle(e.X, clickPoint.Y, (int)w, (int)h);
+                        else rect = new Rectangle(e.X, e.Y, (int)w, (int)h);
+                    }
+                }
+                else if (toolType == PaintTools.DrawLine) //선형 그리기
+                {
+                    rect = new Rectangle(clickPoint.X, clickPoint.Y, clickPoint.X + e.X, clickPoint.Y + e.Y);
+                }
+
+                //리스트에 Rectangle 정보, Tool Type 정보 저장하기
+                listRect.Add(rect);
+                listTool.Add(toolType);
+                DrawBitmap();
+            }
+        }
+        private void DrawBitmap()
+        {
+            if (OriginalBmp != null)
+            {
+                DrawBmp = (Bitmap)OriginalBmp.Clone();
+                for (int i = 0; i < listRect.Count; i++)
+                {
+                    double wRatio = (double)OriginalBmp.Width / pictureBox1.Width;
+                    double hRatio = (double)OriginalBmp.Height / pictureBox1.Height;
+                    Rectangle rect = new Rectangle((int)(listRect[i].X * wRatio), (int)((listRect[i].Y) * hRatio),
+                            (int)(listRect[i].Width * wRatio), (int)(listRect[i].Height * hRatio));
+                    Pen pn = new Pen(Color.Black);
+                    pn.Width = (float)(3 * wRatio);
+
+                    using (Graphics g = Graphics.FromImage(DrawBmp))
+                    {
+                        if (listTool[i] == PaintTools.DrawRectangle) g.DrawRectangle(pn, rect);
+                        else if (listTool[i] == PaintTools.DrawCircle) g.DrawEllipse(pn, rect);
+                        else if (listTool[i] == PaintTools.DrawLine) g.DrawLine(pn, new Point(rect.X, rect.Y), new Point(rect.Width - rect.X, rect.Height - rect.Y));
+                    }
+                }
+                pictureBox1.Image = DrawBmp;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (OriginalBmp != null)
+            {
+                e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                if (listRect.Count > 0 && DrawBmp != null)
+                {
+                    e.Graphics.DrawImage(DrawBmp, imgRect);
+                }
+                else
+                {
+                    e.Graphics.DrawImage(OriginalBmp, imgRect);
+                }
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listRect.Count > 0)
+            {
+                //Undo 실행 취소
+                //맨 마지막 list에 있던 거 temp에 저장하고 마지막 list 삭제
+                tempRect.Add(listRect[listRect.Count - 1]);
+                listRect.RemoveAt(listRect.Count - 1);
+                tempTool.Add(listTool[listTool.Count - 1]);
+                listTool.RemoveAt(listTool.Count - 1);
+                pictureBox1.Refresh();
+                DrawBitmap();
+            }
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tempRect.Count > 0)
+            {
+                //Redo 다시 실행
+                //맨 마지막 temp에 있던 거 list에 저장하고 마지막 temp 삭제
+                listRect.Add(tempRect[tempRect.Count - 1]);
+                tempRect.RemoveAt(tempRect.Count - 1);
+                listTool.Add(tempTool[tempTool.Count - 1]);
+                tempTool.RemoveAt(tempTool.Count - 1);
+                pictureBox1.Refresh();
+                DrawBitmap();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Undo & Redo Shortcut 실행 취소 & 다시 실행 단축키
+            if (e.Control && !e.Shift && e.KeyCode == Keys.Z)
+            {
+                //CTRL + Z : Undo 실행 취소
+                if (listRect.Count > 0)
+                {
+                    //맨 마지막 list에 있던 거 temp에 저장하고 마지막 list 삭제
+                    tempRect.Add(listRect[listRect.Count - 1]);
+                    listRect.RemoveAt(listRect.Count - 1);
+                    tempTool.Add(listTool[listTool.Count - 1]);
+                    listTool.RemoveAt(listTool.Count - 1);
+                    pictureBox1.Refresh();
+                    DrawBitmap();
+                }
+            }
+            else if (e.Control && e.Shift && e.KeyCode == Keys.Z)
+            {
+                //CTRL + SHIFT + Z : Redo 다시 실행
+                if (tempRect.Count > 0)
+                {
+                    //맨 마지막 temp에 있던 거 list에 저장하고 마지막 temp 삭제
+                    listRect.Add(tempRect[tempRect.Count - 1]);
+                    tempRect.RemoveAt(tempRect.Count - 1);
+                    listTool.Add(tempTool[tempTool.Count - 1]);
+                    tempTool.RemoveAt(tempTool.Count - 1);
+                    pictureBox1.Refresh();
+                    DrawBitmap();
+                }
+            }
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PNG 파일 (*.png)|*.png|모든 파일 (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    DrawBmp.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+        }
+
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "PNG 파일 (*.png)|*.png|모든 파일 (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    OriginalBmp = new Bitmap(openFileDialog.FileName);
+                    DrawBmp = (Bitmap)OriginalBmp.Clone();
+                    pictureBox1.Image = DrawBmp;
+                    listRect.Clear();
+                    listTool.Clear();
+                }
+            }
+        }
+    }
+}
